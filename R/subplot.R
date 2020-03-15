6218ff024d0fd9f692c37915effb8737bd3dfa78
mkoohafkan <michael.koohafkan@gmail.com>
2020-03-15 09:15:44 -0700

    added support for subplot theme and NULL plots

--- R/subplot.R
+++ R/subplot.R
@@ -12,7 +12,8 @@ geom_subplot = function(mapping = NULL,
   stat = StatIdentity,
   position = PositionIdentity,
   data = NULL, ...,
-  na.rm = FALSE, inherit.aes = TRUE) {
+	theme = NULL, na.rm = FALSE, inherit.aes = TRUE) {
+
   layer(
     data = data,
     mapping = mapping,
@@ -20,7 +21,7 @@ geom_subplot = function(mapping = NULL,
     geom = GeomSubplot,
     position = position,
     inherit.aes = inherit.aes,
-    params = list(na.rm = na.rm, ...)
+    params = list(na.rm = na.rm, theme = theme, ...)
   )
 }
 
@@ -28,25 +29,25 @@ geom_subplot = function(mapping = NULL,
 GeomSubplot = ggproto("GeomSubplot", Geom,
   required_aes = c("x", "y", "width", "height", "plot"),
 
-non_missing_aes = c("xmin", "xmax", "ymin", "ymax"),
+	non_missing_aes = c("xmin", "xmax", "ymin", "ymax"),
 
 	setup_params = function(data, params) {
 		params$flipped_aes <- has_flipped_aes(data, params)
 		params
 	},
 
-	extra_params = c("na.rm", "orientation"),
+	extra_params = c("na.rm", "orientation", "theme"),
 
 	handle_na = function(data, params) {
 		data
 	},
 
 	setup_data = function(data, params) {
-		data$flipped_aes <- params$flipped_aes
-		data <- flip_data(data, params$flipped_aes)
-		data$width <- data$width %||% params$width
-		data$height <- data$height %||% params$height 
-		data <- transform(data,
+		data$flipped_aes = params$flipped_aes
+		data = flip_data(data, params$flipped_aes)
+		data$width = data$width %||% params$width
+		data$height = data$height %||% params$height
+		data = transform(data,
 											ymin = y - height / 2,
 											ymax = y + height / 2,
 											xmin = x - width / 2,
@@ -57,7 +58,7 @@ non_missing_aes = c("xmin", "xmax", "ymin", "ymax"),
 		flip_data(data, params$flipped_aes)
 	},
 
-  draw_panel = function(self, data, panel_params, coord, na.rm = FALSE) {
+	draw_panel = function(self, data, panel_params, coord, na.rm = FALSE, theme = NULL) {
     n = nrow(data)
     if (n == 1) return(zeroGrob())
     if (!coord$is_linear()) {
@@ -65,14 +66,15 @@ non_missing_aes = c("xmin", "xmax", "ymin", "ymax"),
     }
     #coordinate transformation for viewports
 		coords <- coord$transform(data, panel_params)
-    # plot viewports
-    newGrobs = vector("list", nrow(data))
+		# plot viewports
+		newGrobs = vector("list", nrow(data))
     for (i in seq_along(newGrobs)) {
       vp <- viewport(x = coords$x[i], y = coords$y[i],
 				width = coords$xmax[i] - coords$xmin[i],
 				height = coords$ymax[i] - coords$ymin[i],
         just = c("center", "center"))
-      grob = ggplotGrob(data$plot[[i]])
+			grob = if (is.null(data$plot[[i]])) zeroGrob() else
+        ggplotGrob(data$plot[[i]] + theme)
       newGrobs[[i]] = editGrob(grob,
         vp = vp, name = paste(grob$name, i))
     }
